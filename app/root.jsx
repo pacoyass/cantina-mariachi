1751a8fb3d86dc4bd431de19e431eb6ee49a7d7d
pacoyass <pacoyasix@gmail.com>
2025-08-14 19:51:56 +0100

    root updated stylesheet

--- app/root.jsx
+++ app/root.jsx
@@ -7,7 +7,8 @@ import {
   ScrollRestoration,
 } from "react-router";
 
-import "./app.css";
+import stylesheet from "./app.css?url";
+
 
 export const links = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -20,6 +21,8 @@ export const links = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+  { rel: "stylesheet", href: stylesheet },
+
 ];
 
 export function Layout({ children }) {
